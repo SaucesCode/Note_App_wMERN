341c4a11028f2c03115469e88ceeb706fec20295
Myheadhurts16 <jamessdemesa@gmail.com>
2025-07-19 20:10:56 +0800

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,30 @@
 import express from "express";
+import cors from "cors";
 import notesRouter from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import { rateLimiter } from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 
-connectDB();
-
-//middleware / allows to get access to request body
-app.use(express.json());
+//middleware is a function that runs in the middle of the request and response
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.json()); // allows to get access to request body
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-  console.log(`Server started at ${PORT}`);
+// connect the DB first before starting the server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+  });
 });
---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetail from "./pages/NoteDetail";
+import toast from "react-hot-toast";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/notes/:id" element={<NoteDetail />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
