f1c3c637b078fdd3611c6fb42abb8d399591e4bd
Myheadhurts16 <jamessdemesa@gmail.com>
2025-07-20 17:55:18 +0800

    For deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,33 @@ import notesRouter from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import { rateLimiter } from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware is a function that runs in the middle of the request and response
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json()); // allows to get access to request body
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
 
-const PORT = process.env.PORT || 5001;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/*w", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // connect the DB first before starting the server
 connectDB().then(() => {
