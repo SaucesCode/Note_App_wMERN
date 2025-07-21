c71458f10e7c59e7a8d1b1f2108cc7121ef4ce68
James Patrick De Mesa <83571819+SaucesCode@users.noreply.github.com>
2025-07-21 14:53:48 +0800

    Update upstash.js

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(30, "60 s"),
+  limiter: Ratelimit.slidingWindow(300, "120 s"),
 });
 
 export default ratelimit;
